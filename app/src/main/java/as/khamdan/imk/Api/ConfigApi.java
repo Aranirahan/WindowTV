9691d49931b38f814154691de77374c7755a4f1e
Aran Irahan <32474003+Aranirahan@users.noreply.github.com>
2018-04-17 09:26:16 +0700

    Update ConfigApi.java

--- app/src/main/java/as/khamdan/imk/Api/ConfigApi.java
+++ app/src/main/java/as/khamdan/imk/Api/ConfigApi.java
@@ -7,6 +7,6 @@ public final class ConfigApi {
     private ConfigApi() {
     }
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyBGJZyM5i6mhfI9l9c2WEpDfBPNkL_v3zk";
+    public static final String YOUTUBE_API_KEY = "YOUR API KEY";
 
 }
